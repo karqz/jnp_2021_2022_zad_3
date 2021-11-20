3ce8a3dcaf039580b9e104f4c42d143f3e1ad3c8
Agata Zaleska <az429700@students.mimuw.edu.pl>
2021-11-20 13:10:41 +0100

    teraz działa, wbombilam constexpr do operatorow, nie wiem czy to dobrze ale nie ma clang tidy wtedy

--- fuzzy.h
+++ fuzzy.h
@@ -4,40 +4,43 @@
 #include<iostream>
 #include<set>
 #include<tuple>
-#include<math.h>
+#include<cmath>
+#include <algorithm>
 
 using real_t = double;
 using fuzzy_rank_t = std::tuple<real_t, real_t, real_t>;
 using namespace std;
-
-
 
 class TriFuzzyNum {
     
     public:
 
         constexpr TriFuzzyNum(const real_t &a, const real_t &b, const real_t &c) {
-            real_t nums[] = {a, b, c};
-            sort(begin(nums), end(nums));
+            l = a;
+            m = b;
+            u = c;
+            sortAndUpdateFuzzyValues();
+        }
+
+        TriFuzzyNum(const TriFuzzyNum& that) = default;
+
+        TriFuzzyNum(TriFuzzyNum&& that) = default;
+
+        [[nodiscard]] constexpr real_t lower_value() const { return l; }
+
+        [[nodiscard]] constexpr real_t modal_value() const { return m; }
+
+        [[nodiscard]] constexpr real_t upper_value() const { return u; }
+
+        constexpr void sortAndUpdateFuzzyValues() {
+            std::array<real_t, 3> nums = {l, m, u};
+            std::ranges::sort(nums);
 
             l = nums[0];
             m = nums[1];
             u = nums[2];
         }
 
-        //konstruktor kopiujacy
-        TriFuzzyNum(const TriFuzzyNum& that):l(that.l),m(that.m),u(that.u) {}
-        
-        //konstruktor przenoszacy
-        TriFuzzyNum(TriFuzzyNum&& that):l(move(that.l)),m(move(that.m)),u(move(that.u)) {}
-
-        constexpr real_t lower_value() const { return l; }
-
-        constexpr real_t modal_value() const { return m; }
-
-        constexpr real_t upper_value() const { return u; }
-
-        //operator kopiujacy
         TriFuzzyNum& operator=(const TriFuzzyNum& that) {
             if (this != &that) {
                 l = that.l;
@@ -47,54 +50,49 @@ class TriFuzzyNum {
             return *this;
         }
 
-        //operator przenoszacy
-        TriFuzzyNum& operator=(TriFuzzyNum&& that) {
-            l = move(that.l);
-            m = move(that.m);
-            u = move(that.u);
-            return *this;
-        }
+        constexpr TriFuzzyNum& operator=(TriFuzzyNum&& that) = default;
 
-        TriFuzzyNum& operator+=(const TriFuzzyNum& that) {
+        constexpr TriFuzzyNum& operator+=(const TriFuzzyNum& that) {
             l += that.l;
             m += that.m;
             u += that.u;
             return *this;
         }
 
-        TriFuzzyNum& operator-=(const TriFuzzyNum& that) {
+        constexpr TriFuzzyNum& operator-=(const TriFuzzyNum& that) {
             l -= that.u;
             m -= that.m;
             u -= that.l;
             return *this;
         }
 
-        TriFuzzyNum& operator*=(const TriFuzzyNum& that) {
+        constexpr TriFuzzyNum& operator*=(const TriFuzzyNum& that) {
             l *= that.l;
             m *= that.m;
             u *= that.u;
+            sortAndUpdateFuzzyValues();
             return *this;
         }
 
-        const TriFuzzyNum operator+(const TriFuzzyNum& that) const {
+        constexpr TriFuzzyNum operator+(const TriFuzzyNum& that) const {
             TriFuzzyNum result = *this;
             result += that;
             return result;
         }
 
-        const TriFuzzyNum operator-(const TriFuzzyNum& that) const {
+        constexpr TriFuzzyNum operator-(const TriFuzzyNum& that) const {
             TriFuzzyNum result = *this;
             result -= that;
             return result;
         }
 
-        const TriFuzzyNum operator*(const TriFuzzyNum& that) const {
+        constexpr TriFuzzyNum operator*(const TriFuzzyNum& that) const {
             TriFuzzyNum result = *this;
             result *= that;
             return result;
         }
 
-        constexpr fuzzy_rank_t getRank() const {
+        [[nodiscard]] constexpr fuzzy_rank_t getRank() const {
             real_t z = (u - l) + sqrt(1 + (u - m) * (u - m)) + sqrt(1 + (m - l) * (m - l));
             real_t x = ((u - l) * m + sqrt(1 + (u - m) * (u - m)) * l + sqrt(1 + (m - l) * (m - l)) * u) / z;
             real_t y = (u - l) / z;
@@ -110,25 +108,12 @@ class TriFuzzyNum {
             return !(*this == that);
         }
 
-        auto operator<=>(const int& a) const;
+        //auto operator<=>(const int& a) const;
         constexpr auto operator<=>(const TriFuzzyNum& that) const {
             fuzzy_rank_t rank1 = getRank();
             fuzzy_rank_t rank2 = that.getRank();
 
-            real_t first_1 = get<0>(rank1), second_1 = get<1>(rank1), third_1 = get<2>(rank1);
-            real_t first_2 = get<0>(rank2), second_2 = get<1>(rank2), third_2 = get<2>(rank2);
-
-            if (first_1 > first_2) return strong_ordering::greater;
-            else if (first_2 > first_1) return strong_ordering::less;
-            else {
-                if (second_1 > second_2) return strong_ordering::greater;
-                else if (second_2 > second_1) return strong_ordering::less;
-                else {
-                    if (third_1 > third_1) return strong_ordering::greater;
-                    else if (third_2 > third_1) return strong_ordering::less;
-                    else return strong_ordering::equal;
-                }
-            }
+            return rank1 <=> rank2;
         }
 
         friend std::ostream& operator<<(std::ostream& os, const TriFuzzyNum& num) {
@@ -143,19 +128,19 @@ class TriFuzzyNum {
         real_t l, m, u;
 };
 
-constinit TriFuzzyNum crisp_zero = TriFuzzyNum(0, 0, 0);
-
 consteval TriFuzzyNum crisp_number(real_t v) {
-    return TriFuzzyNum(v, v, v);
+    return {v, v, v};
 }
+
+inline constinit const TriFuzzyNum crisp_zero = crisp_number(0);
 
 class TriFuzzyNumSet {
     public:
-        TriFuzzyNumSet(initializer_list<TriFuzzyNum> args) {
-            set = multiset<TriFuzzyNum>(args.begin(), args.end());
-        }
+        TriFuzzyNumSet():set(multiset<TriFuzzyNum>()) {}
 
-        TriFuzzyNumSet(const TriFuzzyNumSet& that):set(that.set) {}
+        TriFuzzyNumSet(initializer_list<TriFuzzyNum> args):set(args) {}
+
+        TriFuzzyNumSet(const TriFuzzyNumSet& that) = default;
 
         TriFuzzyNumSet(TriFuzzyNumSet&& that):set(move(that.set)) {}
 
@@ -171,7 +156,7 @@ class TriFuzzyNumSet {
             return *this;
         }
 
-        ~TriFuzzyNumSet() {}
+        ~TriFuzzyNumSet() = default;
 
         void insert(const TriFuzzyNum& number) {
             set.insert(number);
@@ -185,11 +170,11 @@ class TriFuzzyNumSet {
             set.erase(number);
         }
 
-        void remove(TriFuzzyNum&& number) {
-            set.erase(move(number));
-        }
-
         TriFuzzyNum arithmetic_mean() {
+            if (set.empty()) {
+                throw std::length_error("TriFuzzyNumSet::arithmetic_mean - the set is empty.");
+            }
+
             TriFuzzyNum result = crisp_zero;
             real_t size = 0;
             for (TriFuzzyNum number : set) {
